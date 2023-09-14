52b33232c0af4a5293c3fce7b342b49dea62c89d
eyhdev <alaqsa335@gmail.com>
2023-09-14 19:41:08 +0300

    Initial Commit

---
+++ AnimalApp/AnimalAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalAppApp.swift
+//  AnimalApp
+//
+//  Created by eyh.mac on 14.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct AnimalAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
